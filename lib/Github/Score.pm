ed5efa62adffc4a01d9671aa974c42f8d2318fdd
canardee-io <canardee.io@gmail.com>
2011-12-04 12:52:55 +0000

    First contact with Dist::Zilla

--- lib/Github/Score.pm
+++ lib/Github/Score.pm
@@ -47,33 +47,23 @@ package Github::Score;
  
  1;
  
- use Test::More tests => 15;
- 
- BEGIN { use_ok 'Github::Score'; }
- 
- {
-     isa_ok( my $gs = Github::Score->new( user => 'stevan', repo => 'ox' ), 'Github::Score' );
-     is( $gs->user, 'stevan' );
-     is( $gs->repo, 'ox' );
- }
- 
- {
-     isa_ok( my $gs = Github::Score->new( { user => 'stevan', repo => 'ox' } ), 'Github::Score' );
-     is( $gs->user, 'stevan' );
-     is( $gs->repo, 'ox' );
- }
- 
- {
-     isa_ok( my $gs = Github::Score->new( url => 'stevan/ox' ), 'Github::Score' );
-     is( $gs->user, 'stevan' );
-     is( $gs->repo, 'ox' );
- }
- 
- {
-     isa_ok( my $gs = Github::Score->new('stevan/ox'), 'Github::Score' );
-     is( $gs->user, 'stevan' );
-     is( $gs->repo, 'ox' );
-     isa_ok( my $scores = $gs->scores, 'HASH' );
-     ok( scalar %$scores );
- }
+# ABSTRACT: a really awesome library
+##
+=head1 SYNOPSIS
+use Github::Score;
 
+...
+
+=method method_x
+
+This method does something experimental.
+
+=method method_y
+
+This method returns a reason.
+
+=head1 SEE ALSO
+
+=for :list
+* L<Your::Module>
+* L<Your::Package>
